21a2fdf467b5a8ad994490064b147aad5020f519
Thiago <thiago@tribecca.com.br>
2019-01-14 18:27:25 -0200

    salvando também

--- my-app/src/app/hero.service.old.ts
+++ my-app/src/app/hero.service.old.ts
@@ -8,7 +8,6 @@ import { Hero } from './hero';
 import { MessageService } from './message.service';
 
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-
 /** The number of widgets present */
 declare var firebase: any;
 const httpOptions = {
@@ -26,60 +25,25 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService,
     private _afs: AngularFirestore) {
-      this.itemsCollection = _afs.collection<Hero>('heroes');
       this.afs = _afs;
     }
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     this.log(`fetched heroes`);
-    return this.afs.collection<Hero>('heroes')
-    .valueChanges();
+    return this.afs.collection<Hero>('heroes').valueChanges();
   }
   /** GET hero by id. Will 404 if id not found */
-  getHero(id: number): Observable<Hero> {
-    return this.afs
-    .collection('heroes', ref => ref.where('id', '==', id).limit(1))
-    .valueChanges().pipe(
-      tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<any>('updateHero'))
-    );
+  getHero(id: number): Observable<any[]> {
+    this.log(`fetched hero id=${id}`);
+    return this.afs.collection<any[]>('heroes', ref => ref.where('id', '==', id).limit(1)).valueChanges();
   }
 
-  /** POST: add a new hero to the server */
-  addHero (hero: Hero): Observable<Hero> {
-    this.log(`added hero w/ id=${hero.id}`);
-    const id = this.afs.createId();
-    this.itemsCollection.doc(id).set(hero);
-    return this.getHero(hero.id);
-  }
-  /** PUT: update the hero on the server */
-  updateHero (hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
-      tap(_ => this.log(`updated hero id=${hero.id}`)),
-      catchError(this.handleError<any>('updateHero'))
-    );
-  }
-  
-  /** DELETE: delete the hero from the server */
-  deleteHero (hero: Hero | number): Observable<Hero> {
-    const id = typeof hero === 'number' ? hero : hero.id;
+  /** GET hero by id. Will 404 if id not found */
+  getHeroOld(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
-
-    return this.http.delete<Hero>(url, httpOptions).pipe(
-      tap(_ => this.log(`deleted hero id=${id}`)),
-      catchError(this.handleError<Hero>('deleteHero'))
-    );
-  }
-
-  /* GET heroes whose name contains search term */
-  searchHeroes(term: string): Observable<Hero[]> {
-    if (!term.trim()) {
-      // if not search term, return empty hero array.
-      return of([]);
-    }
-    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
-      tap(_ => this.log(`found heroes matching "${term}"`)),
-      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
 
@@ -105,5 +69,44 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
   }
 }
